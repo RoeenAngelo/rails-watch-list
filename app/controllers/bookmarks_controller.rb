95c91bd318d3782b9ea40d95d343ffb74a61d786
Roeen Acal <roeenacal@gmail.com>
2024-02-24 14:10:07 +0800

    fix add movie to list feature

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,6 @@
 class BookmarksController < ApplicationController
-  before_action :set_list, only: [:create, :new]
   before_action :set_bookmark, only: :destroy
+  before_action :set_list, only: [:new, :create]
 
   def new
     @bookmark = Bookmark.new
@@ -23,15 +23,15 @@ class BookmarksController < ApplicationController
 
   private
 
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
   def set_bookmark
     @bookmark = Bookmark.find(params[:id])
   end
 
   def set_list
     @list = List.find(params[:list_id])
-  end
-
-  def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
